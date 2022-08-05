f4f8365e612c994114e5451620d56031b3ef1deb
Ahmed Emam <ahmedemam576@gmail.com>
2022-08-05 17:15:30 +0200

    we build the generator class using the building blocks, and the init to enable  importing

---
+++ __init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  5 16:06:56 2022
+
+@author: ahmedemam576
+"""
+
+from . import expanding_block
+from . import contracting_block
+from . import residual_block
+from . import feature_map
+
+__all__ = ['expanding_block','contracting_block', 'residual_block', 'feature_map' ]
--- generator.py
+++ generator.py
@@ -1,9 +1,70 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug  3 11:31:13 2022
+Created on Fri Aug  5 16:09:09 2022
 
 @author: ahmedemam576
 """
+import torch
+from torch import nn
 
-class Generator(nn.module):
+from expanding_block import Expanding_Block
+#the expanding block divides the number of the filters by two, output_filters = input_filters//2
+from contracting_block import Contracting_Block
+#the contracting block doubles the number of the filters, output_filters = 2 * input_filters
+
+from residual_block import Residual_Block
+# the residual block consists of 2 convolution layers, and at the end we concatenate the input to the output
+# also the input filters and the output filters are equal 
+from feature_map import Feature_map
+# the feature map class doesn't change the number of the filters , it's just a convolutional layer
+
+class Generator(nn.Module):
+    def __init__(self, input_channels, output_channels, hidden_channels=64):
+        
+        super(Generator, self).__init__()
+        self.up_feature =Feature_map(input_channels, hidden_channels)
+        
+        
+        
+        self.contract1 = Contracting_Block(hidden_channels)
+        
+        self.contract2 = Contracting_Block(hidden_channels*2)
+        
+        self.res1 = Residual_Block(hidden_channels*4)
+        self.res2 = Residual_Block(hidden_channels*4)
+        self.res3 = Residual_Block(hidden_channels*4)
+        self.res4 = Residual_Block(hidden_channels*4)
+        self.res5 = Residual_Block(hidden_channels*4)
+        self.res6 = Residual_Block(hidden_channels*4)
+        self.res7 = Residual_Block(hidden_channels*4)
+        self.res8 = Residual_Block(hidden_channels*4)
+        self.res9 = Residual_Block(hidden_channels*4)
+        
+        self.expand1 = Expanding_Block(hidden_channels*4)
+        self.expand2 = Expanding_Block(hidden_channels*2)
+        
+        self.down_feature = Feature_map(hidden_channels, output_channels)
+        self.tanh = nn.Tanh()
+        
+    def Forward(self, x):
+        x= self.up_feature(x)
+        x= self.contract1(x)
+        x= self.contract2(x)
+        x= self.res1(x)
+        x= self.res2(x)
+        x= self.res3(x)
+        x= self.res4(x)
+        x= self.res5(x)
+        x= self.res6(x)
+        x= self.res7(x)
+        x= self.res8(x)
+        x= self.res9(x)
+        x= self.expand1(x)
+        x= self.expand2(x)
+        x= self.down_feature(x)
+        x= self.tanh(x)
+        return x
+    
+    
+    
