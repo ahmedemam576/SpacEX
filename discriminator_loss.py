8fddc4e5819ef3f2167026b5ecfc14d097b52b0a
Ahmed Emam <ahmedemam576@gmail.com>
2022-08-08 17:05:41 +0200

    discriminator_loss
    created a discriminator loss , to calculate the disc loss, init. the class and call it with the appropriate  it'sweight

--- discriminator_loss.py
+++ discriminator_loss.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug  3 11:33:23 2022
+Created on Mon Aug  8 16:33:01 2022
 
 @author: ahmedemam576
 """
+import torch
 
-class Discriminator_Loss(nn.module):
+class Discriminator_loss():
+    
+    '''
+    real_x: oringinal image
+    fake_x: a generated image from the generator..gen(AB)-->B`
+    disc_x: a discriminatorfor the original image
+    loss_type: L1 loss or MSELoss
+    
+    '''
+    def __init__(self, real_X, fake_X, disc_X, loss_type ):
+        self.real_X = real_X
+        self.fake_X = fake_X
+        self.disc_X = disc_X
+        self.loss_type = loss_type
+        
+        
+    def __call__(self):    
+        disc_fake_x = disc_X(fake_X.detach()) # detach the generator, not to optimize the generator by the disc. objective function
+        disc_fake_loss = loss_type(disc_fake_loss, torch.zeros_like(disc_fake_loss))
+        disc_real_x = disc_X(real_X)
+        disc_real_loss = loss_type(disc_real_loss, torch.ones_like(disc_real_loss))
+        avg_disc_loss= 0.5*(disc_fake_loss + disc_real_loss)
+        
+        return avg_disc_loss
+    
     
