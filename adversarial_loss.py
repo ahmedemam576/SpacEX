2b3b8bc130a5c7e85eab43b9f34ed798f7e07d79
Ahmed Emam <ahmedemam576@gmail.com>
2022-08-09 11:34:43 +0200

    started the adversarial loss

---
+++ adversarial_loss.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 10:43:26 2022
+
+@author: ahmedemam576
+"""
+from gan_loss_term import Gan_loss_term
+
+class Adverserial_Loss(Gan_loss_term):
