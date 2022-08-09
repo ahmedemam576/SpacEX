ba83afbb0750c013af220918fac69a9eb993662b
Ahmed Emam <ahmedemam576@gmail.com>
2022-08-09 17:25:34 +0200

    added the new losses to the library

--- __init__.py
+++ __init__.py
@@ -15,9 +15,10 @@ from . import generator_loss
 from . import patch_discriminator
 from . import discriminator_loss
 from . import gan_loss_term
+from. import adversarial_loss, identity_loss, cycle_loss
 __all__ = [
     'expanding_block','contracting_block', 'residual_block',
     'feature_map' 
      ,'generator','patch_discriminator', 'generator_loss', 'discriminator_loss'
-     ;'gan_loss_term'
+     ,'gan_loss_term', 'adversarial_loss', 'identity_loss', 'cycle_loss'
     ]
