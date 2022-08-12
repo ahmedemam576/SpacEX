929f207cfd925f4afdff9272d754bbabdbc188bb
Ahmed Emam <ahmedemam576@gmail.com>
2022-08-12 12:56:05 +0200

    edited the generator_loss term , and added all the loss to the generator loss , except the am loss term

--- gan_loss_term.py
+++ gan_loss_term.py
@@ -5,22 +5,26 @@ Created on Tue Aug  9 10:53:03 2022
 
 @author: ahmedemam576
 """
-from torch.nn  import L1Loss, MSELoss
+
 class Gan_loss_term:
     '''
     define the main building blocks used in many loss terms
     which is used commonly in GANs application
     ----> a constructor for other GANs loss functions
     '''
-    def __init__ (self, real_X,real_Y, generator_XY, generator_YX, discriminator_X, discriminator_Y):
+    def __init__ (self, real_X, real_Y, gen_XY, gen_YX, disc_X, disc_Y,  adv_norm, identity_norm, cycle_norm):
         
       
-        self. generator = generator_YX
-        self. generator = generator_XY
-        self. discriminator = discriminator_Y
-        self. discriminator = discriminator_X
-        self.real_x = real_X
-        self.real_x = real_Y
+        self.gen_YX = gen_YX
+        self.gen_XY = gen_XY
+        self.disc_Y = disc_Y
+        self. disc_X = disc_X
+        self.real_X = real_X
+        self.real_Y = real_Y
+        self.adv_norm = adv_norm
+        self. identity_norm= identity_norm
+        self.cycle_norm = cycle_norm
+        
         print(f'{self.name} is created')
         
         
--- generator_loss.py
+++ generator_loss.py
@@ -14,17 +14,25 @@ from gan_loss_term import Gan_loss_term
 class Generator_Loss(Gan_loss_term):
     def __init__(self, real_X, real_Y, gen_XY, gen_YX, disc_X, disc_Y, adv_norm, identity_norm, cycle_norm):
         super(Generator_Loss,self).__init__()
+        self.adverserial_loss(real_X, real_Y, gen_XY, gen_YX, disc_X, disc_Y)
+        self.identity_loss(real_X, real_Y, gen_XY, gen_YX)
+        self.cycle_loss(real_X, real_Y, gen_XY, gen_YX)
+        
+        
+        
+        self.sum_adv_loss, _, _ , self.fake_X, self.fake_Y= self.adverserial_loss(self.real_X, self.real_Y, self.gen_XY, self.gen_YX, self.disc_X, self.disc_Y)
+        self.sum_identity_loss = self.identity_loss(self.real_X, self.real_Y, self.gen_XY, self.gen_YX)
+        self.sum_cycle_loss = self.cycle_loss(self.real_X, self.real_Y, self.gen_XY, gen_YX)
     
-    
-    def adverserial_loss (self,real_X, real_Y,gen_XY, gen_YX, disc_X, disc_Y, adv_norm):
+    def adverserial_loss(self, real_X, real_Y,gen_XY, gen_YX, disc_X, disc_Y):
         
         fake_Y =gen_XY(real_X)
         disced_fake_y = disc_Y(fake_Y)
-        adv_loss_Y = (adv_norm(disced_fake_y, torch.ones_like(disced_fake_y)))
+        adv_loss_Y = (self.adv_norm(disced_fake_y, torch.ones_like(disced_fake_y)))
         
         fake_X =gen_YX(real_Y)
         disced_fake_X = disc_X(fake_X)
-        adv_loss_X = (adv_norm(disced_fake_X, torch.ones_like(disced_fake_X)))
+        adv_loss_X = (self.adv_norm(disced_fake_X, torch.ones_like(disced_fake_X)))
         sum_adv_loss = (adv_loss_X+adv_loss_Y)
         
       
@@ -34,16 +42,34 @@ class Generator_Loss(Gan_loss_term):
     
     
     def identity_loss(self, real_X,real_Y, gen_XY, gen_YX):
-        id_x = gen_YX(real_x)
-        id_y = gen_XY(real_y)
+        id_x = gen_YX(real_X)
+        id_y = gen_XY(real_Y)
         sum_id_loss = id_x+id_y
         return sum_id_loss
     
-    def __call__(adv_weight, id_weight, cycle_weight):
+    
+    
+   #' we use the adverserial_loss function to get the fake_x and fake_y used in the cycle loss'
+    
+    
+    def cycle_loss(self, real_X,real_Y, gen_XY,gen_YX):
         
-        adverserial_loss =
-        identity_loss =
-        cycle_loss=
+        fake_y= self.fake_Y
+        cycle_XY = self.cycle_norm(real_Y, fake_y)
+        fake_x = self.fake_X
+        cycle_YX = self.cycle_norm(fake_x, real_X)
+        sum_cycle_loss = cycle_XY+cycle_YX
+        return sum_cycle_loss
+    
+    
+    def AM_loss(self, classifier):
+        pass
+    
+    def __call__(self, adv_weight, id_weight, cycle_weight):
+        
+        x = (adv_weight * self.sum_adv_loss) + (id_weight * self.sum_identity_loss) + (cycle_weight * self.sum_cycle_loss)
+        return x
+                
         
         
         
@@ -52,4 +78,4 @@ class Generator_Loss(Gan_loss_term):
         
         
         
-        adv_weight* adverserial_loss + id_weight*identity_loss+ cycle_weight* cycle_loss
+        
