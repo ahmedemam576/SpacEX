156dcf4dc4aa0536033549e24497dea3a4f0f32c
Ahmed Emam <ahmedemam576@gmail.com>
2022-08-17 15:12:18 +0200

    we need to edit the train function to use the generator loss class

--- train.py
+++ train.py
@@ -6,13 +6,18 @@ Created on Wed Aug  3 11:36:11 2022
 @author: ahmedemam576
 """
 import torch
+import torchvision
+from torch import nn
 from generator import Generator
 from patch_discriminator import Patch_Discriminator
 import glob
 import random
 import os
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from PIL import Image
+from tqdm.auto import tqdm
+from torchvision import transforms
 
 
 class ImageDataset(Dataset):
@@ -50,6 +55,31 @@ class ImageDataset(Dataset):
         return min(len(self.files_A), len(self.files_B))
 
 
+
+adv_criterion = nn.MSELoss() 
+recon_criterion = nn.L1Loss() 
+
+n_epochs = 20
+dim_A = 3
+dim_B = 3
+display_step = 200
+batch_size = 1
+lr = 0.0002
+load_shape = 286
+target_shape = 256
+device = 'cuda'
+
+transform = transforms.Compose([
+    transforms.Resize(load_shape),
+    transforms.RandomCrop(target_shape),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
+
+
+dataset = ImageDataset("horse2zebra", transform=transform)
+
+
 # define  training parameters
 a_dim = 3
 b_dim =3
@@ -71,3 +101,81 @@ disc_B = Patch_Discriminator(b_dim).to(device)
 gen_opt = torch.optim.Adam(list(gen_AB.parameters())+gen_BA.parameters(), lr=learning_rate, betas=(0.5,0.999))
 disc_A_opt = torch.optim.Adam(disc_A.parameters(), lr=learning_rate, betas=(0.5,0.999))
 disc_B_opt = torch.Optional.Adam(disc_B.parameters(), lr= learning_rate, betas=(0.5,0.999))
+
+
+
+
+
+''' you need to adapt this code into your architechture'''
+
+
+def train(save_model=False):
+    mean_generator_loss = 0
+    mean_discriminator_loss = 0
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    cur_step = 0
+
+    for epoch in range(n_epochs):
+        # Dataloader returns the batches
+        # for image, _ in tqdm(dataloader):
+        for real_A, real_B in tqdm(dataloader):
+            
+            # image_width = image.shape[3]
+            real_A = nn.functional.interpolate(real_A, size=target_shape)
+            real_B = nn.functional.interpolate(real_B, size=target_shape)
+            cur_batch_size = len(real_A)
+            real_A = real_A.to(device)
+            real_B = real_B.to(device)
+
+            ### Update discriminator A ###
+            disc_A_opt.zero_grad() # Zero out the gradient before backpropagation
+            with torch.no_grad():
+                fake_A = gen_BA(real_B)
+            disc_A_loss = get_disc_loss(real_A, fake_A, disc_A, adv_criterion)
+            disc_A_loss.backward(retain_graph=True) # Update gradients
+            disc_A_opt.step() # Update optimizer
+
+            ### Update discriminator B ###
+            disc_B_opt.zero_grad() # Zero out the gradient before backpropagation
+            with torch.no_grad():
+                fake_B = gen_AB(real_A)
+            disc_B_loss = get_disc_loss(real_B, fake_B, disc_B, adv_criterion)
+            disc_B_loss.backward(retain_graph=True) # Update gradients
+            disc_B_opt.step() # Update optimizer
+
+            ### Update generator ###
+            gen_opt.zero_grad()
+            gen_loss, fake_A, fake_B = get_gen_loss(
+            real_A, real_B, gen_AB, gen_BA, disc_A, disc_B, adv_criterion, recon_criterion, recon_criterion
+            )
+            gen_loss.backward() # Update gradients
+            gen_opt.step() # Update optimizer
+
+            # Keep track of the average discriminator loss
+            mean_discriminator_loss += disc_A_loss.item() / display_step
+            # Keep track of the average generator loss
+            mean_generator_loss += gen_loss.item() / display_step
+
+            ### Visualization code ###
+            if cur_step % display_step == 0:
+                print(f"Epoch {epoch}: Step {cur_step}: Generator (U-Net) loss: {mean_generator_loss}, Discriminator loss: {mean_discriminator_loss}")
+                show_tensor_images(torch.cat([real_A, real_B]), size=(dim_A, target_shape, target_shape))
+                show_tensor_images(torch.cat([fake_B, fake_A]), size=(dim_B, target_shape, target_shape))
+                mean_generator_loss = 0
+                mean_discriminator_loss = 0
+                # You can change save_model to True if you'd like to save the model
+                if save_model:
+                    torch.save({
+                        'gen_AB': gen_AB.state_dict(),
+                        'gen_BA': gen_BA.state_dict(),
+                        'gen_opt': gen_opt.state_dict(),
+                        'disc_A': disc_A.state_dict(),
+                        'disc_A_opt': disc_A_opt.state_dict(),
+                        'disc_B': disc_B.state_dict(),
+                        'disc_B_opt': disc_B_opt.state_dict()
+                    }, f"cycleGAN_{cur_step}.pth")
+            cur_step += 1
+            
+if __name__ == '__main__':
+    
+    train()
