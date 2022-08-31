c72809c93a76adf1ff8a014d64f2d64c0e87ef24
Timo Tjaden Stomberg <timo.stomberg@uni-bonn.de>
2022-08-31 17:24:17 +0200

    asos runs but real_A and real_B in dataset is specified arbitrary

---
+++ setup.sh
@@ -0,0 +1,13 @@
+conda deactivate
+conda create --name spacex python=3.9
+conda activate spacex
+
+conda install numpy pandas scikit-learn matplotlib tqdm
+
+conda install -c pytorch pytorch torchvision torchaudio cudatoolkit=11.3
+conda install -c conda-forge tensorboard
+
+conda install gdal
+conda install -c conda-forge earthengine-api
+pip install git+https://gitlab.jsc.fz-juelich.de/kiste/asos@main
+
